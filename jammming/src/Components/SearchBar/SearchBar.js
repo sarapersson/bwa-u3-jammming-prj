ef9aec8071dce8020ef1e7ad6e379c3eeddbb806
sarapersson <30837182+sarapersson@users.noreply.github.com>
2017-10-07 00:30:52 +0200

    Codecademy generated from the project submission page.

---
+++ jammming/src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    };
+
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  handleTermChange(e) {
+    this.setState({term: e.target.value});
+  }
+
+  search() {
+    this.props.onSearch(this.state.term);
+  }
+
+  render() {
+    return (
+      <div className="SearchBar">
+        <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
+        <a onClick={this.search}>SEARCH</a>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
